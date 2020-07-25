d219ae824494d078eabbd0934a7dbcaba1bfef87
Mikhail Boldyrev <miboldyrev@gmail.com>
2020-07-25 12:30:21 -0500

    speedup 1.1.3 -> 1.2.0
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- migration_data/release_1_2_0.py
+++ migration_data/release_1_2_0.py
@@ -1,5 +1,6 @@
 import itertools
 import json
+import multiprocessing
 
 
 def migrate_1_1_3_to_1_2_0(cursor, block_storage):
@@ -34,24 +35,36 @@ def migrate_1_1_3_to_1_2_0(cursor, block_storage):
     cursor.execute(
         'create table blockstorage_data (h bigint, i bigint, t bigint)')
 
-    def get_blocks_data(blocks):
-        for block in blocks:
-            height = block.block_v1.payload.height
-            for index, tx in enumerate(block.block_v1.payload.transactions):
-                yield {
-                    'h': height,
-                    'i': index,
-                    't': tx.payload.reduced_payload.created_time
-                }
+    def get_block_data(block, output_queue):
+        height = block.block_v1.payload.height
+        for index, tx in enumerate(block.block_v1.payload.transactions):
+            output_queue.put(
+                (height, index, tx.payload.reduced_payload.created_time))
 
-    BULK_SZ = 1000
-    it = iter(get_blocks_data(block_storage.iterate()))
-    while True:
-        chunk = tuple(itertools.islice(it, BULK_SZ))
-        if len(chunk) == 0: break
-        cursor.execute(
-            'insert into blockstorage_data (select * from json_populate_recordset(null::blockstorage_data, %s))',
-            (json.dumps(chunk), ))
+    def submit_block_data(block_data_to_submit):
+        BULK_SZ = 1000
+        data = iter(block_data_to_submit.get, 'stop')
+        while True:
+            chunk = tuple(itertools.islice(data, BULK_SZ))
+            if len(chunk) == 0: break
+            cursor.execute(
+                'insert into blockstorage_data (h, i, t) values {}'.format(
+                    ', '.join(map(str, chunk))))
+
+    multiprocessing_manager = multiprocessing.Manager()
+    block_data_to_submit = multiprocessing_manager.Queue()
+
+    block_data_submitter = multiprocessing.Process(
+        target=submit_block_data, args=(block_data_to_submit, ))
+    block_data_submitter.start()
+
+    # I could not parallelize block loading, but it would be nice
+    for block in block_storage.iterate():
+        get_block_data(block, block_data_to_submit)
+
+    block_data_to_submit.put('stop')
+
+    block_data_submitter.join()
 
     # now merge the tables and block storage data
     cursor.execute('''
@@ -63,16 +76,6 @@ def migrate_1_1_3_to_1_2_0(cursor, block_storage):
             height bigint,
             index bigint
         );
-        CREATE INDEX IF NOT EXISTS tx_positions_hash_index
-            ON tx_positions
-            USING hash
-            (hash);
-        CREATE INDEX IF NOT EXISTS tx_positions_creator_id_asset_index
-            ON tx_positions
-            (creator_id, asset_id);
-        CREATE INDEX IF NOT EXISTS tx_positions_ts_height_index_index
-            ON tx_positions
-            (ts);
 
         -- aux constraint to avoid duplicate rows
         alter table tx_positions
@@ -126,6 +129,18 @@ def migrate_1_1_3_to_1_2_0(cursor, block_storage):
         -- drop aux stuff
         drop table blockstorage_data;
         alter table tx_positions drop constraint aux_tx_positions_unique_constraint cascade;
+
+        -- create indices
+        CREATE INDEX IF NOT EXISTS tx_positions_hash_index
+            ON tx_positions
+            USING hash
+            (hash);
+        CREATE INDEX IF NOT EXISTS tx_positions_creator_id_asset_index
+            ON tx_positions
+            (creator_id, asset_id);
+        CREATE INDEX IF NOT EXISTS tx_positions_ts_height_index_index
+            ON tx_positions
+            (ts);
             ''')
 
     # burrow stuff
